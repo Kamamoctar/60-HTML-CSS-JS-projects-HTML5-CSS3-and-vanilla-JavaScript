28f232b7c645727bc72d4666e0559726c2f53a0d
NAMADOU MOCTAR KAMAKATE <kamamoctar@gmail.com>
2024-07-23 19:03:36 +0000

    Fini

--- Age calculator/app.js
+++ Age calculator/app.js
@@ -1,8 +1,24 @@
 const buttuonEl = document.getElementById('btn')
 const birthdayEl = document.getElementById('birthday')
+const resultEl = document.getElementById('result')
 
 
 // Les fonctions
+/**
+ * Calculates the age based on the given birthday value.
+ *
+ * @param {string} birthdayValue - The birthday value in the format 'YYYY-MM-DD'.
+ * @return {number|undefined} The calculated age or undefined if the birthday value is empty.
+ */
+function calculateAge(){
+    const birthdayValue = birthdayEl.value;
+    if(birthdayValue === ''){
+        alert('Please enter a date of birth')
+    }else{
+        const age = getAge(birthdayValue)
+        resultEl.innerText = `Your age is ${age} years old`
+    }
+}
 /**
  * Calculates the age based on the given birthday value.
  *
@@ -15,19 +31,12 @@ function getAge(birthdayValue){
 
     let age = currentDate.getFullYear() - birthDate.getFullYear()
     
-    const month = currentDate.getMonth() - birthDate.getMonth()
-    if(month < 0 || (month === 0 && currentDate.getDate() < birthDate.getDate())){
+// Calcules the month difference from current date in time
+    const monthDiff = currentDate.getMonth() - birthDate.getMonth()
+    if(monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate())){
         age--
     }
    return age
-   console.log(age)
 }
 
-buttuonEl.addEventListener('click', ()=>{
-    const birthdayValue = birthdayEl.value;
-    if(birthdayValue === ''){
-        alert('Please enter a date of birth')
-    }else{
-        const age = getAge(birthdayValue)
-    }
-})
+buttuonEl.addEventListener('click', calculateAge)
